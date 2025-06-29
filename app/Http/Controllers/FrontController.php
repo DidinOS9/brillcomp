072feb56cac77347b30597f2db9e93ce59189663
DidinOS09 <didinoktaviandasetiawan@gmail.com>
2025-06-29 23:48:43 +0700

    feat(frontend): setup master layout and implement initial homepage view

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CompanyStatistic;
+use App\Models\HeroSection;
+use App\Models\OurPrinciple;
+use App\Models\OurTeam;
+use App\Models\Product;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    public function index(){
-        return view('front.index');
+    public function index()
+    {
+        //
+        $hero_section = HeroSection::orderByDesc('id')->take(1)->get();
+        $statistics = CompanyStatistic::take(4)->get();
+        $principles = OurPrinciple::take(4)->get();
+        $products = Product::take(3)->get();
+        $teams = OurTeam::take(7)->get();
+        $testimonials = Testimonial::take(4)->get();
+        return view('front.index', compact('hero_section','statistics', 'principles', 'products', 'teams', 'testimonials'));
     }
 }
